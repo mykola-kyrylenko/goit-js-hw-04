d697e0971b327cc7258bed424e3cba66675b978d
Mykola Kyrylenko <bulletbest@gmail.com>
2020-08-14 21:23:32 +0300

    call

--- JS/task-2.js
+++ JS/task-2.js
@@ -16,7 +16,7 @@ const inventory = {
 
 const invokeInventoryAction = function (itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  action.bind(inventory)(itemName);
+  action.call(inventory, itemName);
 };
 
 invokeInventoryAction("Medkit", inventory.add);
